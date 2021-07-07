4a05cab8ed9b2f205e85902ba4551fbfb89c8f2e
Gaev Cenne <gaev@evilparadise.com>
2021-07-07 17:14:01 +0200

    update: adding the second route

--- api/server.js
+++ api/server.js
@@ -8,8 +8,45 @@ app.use(cors())
 
 
 app.listen(port, () => {
-    
-    app.get('/all', (req, res) => {
+
+    app.get('/all', (_, res) => {
         res.send(data);
+    });
+
+    app.get('/country/:name', (rCountry, res) => {
+        let result
+
+        for (i in data) {
+
+            if (rCountry.params.name === data[i].name ||
+                rCountry.params.name === data[i].alpha2Code || rCountry.params.name === data[i].alpha3Code
+            ) {
+                result =res.send(data[i])
+
+            }
+        }
+
+        if (result === undefined) {
+            res.status(404)
+        }
+
+        res.end()
+    });
+
+    app.get('/capital/:name', (rCountry, res) => {
+        let result
+
+        for (i in data) {
+
+            if (rCountry.params.name === data[i].capital) {
+                result =res.send(data[i])
+            }
+        }
+
+        if (result === undefined) {
+            res.status(404)
+        }
+
+        res.end()
     });
 });
--- front/js/script.js
+++ front/js/script.js
@@ -1,31 +1,40 @@
 $(() => {
     let countries
     
-    $.ajax("http://127.0.0.1:3600/all")
-        .done((data) => {
+    $("button").click(() => {
+        
+        if (!$("#userSearch").val()) {
+            $("#userSearch").hasClass("is-valid") ? $("input").removeClass("is-valid") : $("#userSearch").addClass("is-invalid")
+            $("#helpText").html("Merci de renseigner un pays ou une capitale")
+            $("#countryResult")[0].hidden ? $("#countryResult").show() : () => {};
+        } else {
+            let radio = $(":radio")
 
-            $("#countriesList").append("<ul>")
-            for (i in data) {
-                let country =data[i]
-                let currencies ="<li>Devises <ul>"
+            for (let i =0; i < radio.length; i++) {
+                if (radio[i].checked) {
 
-                for (j in country.currencies) {
-                    currencies += `<li>${(typeof country.currencies[j].symbol === "string" ? country.currencies[j].symbol : "N/C")} (${country.currencies[j].name})</li>`
+                    $.ajax(`http://127.0.0.1:3600/${$(":radio")[i].defaultValue}/${$("input").val()}`)
+                    .done(data => {
+                        !$("#countryResult")[0].hidden ? $("#countryResult").hide() : () => {};
+                        $("#userSearch").hasClass("is-invalid") ? $("input").removeClass("is-invalid") : $("#userSearch").addClass("is-valid")
+                        
+                        $("#country").html(`<h3>${data.name}</h3>
+                        <ul>
+                            <li>Capitale : ${data.capital}</li>
+                            <li>Région : ${data.region}</li> 
+                        </ul>
+                        `)
+                    })
+                    .fail(() => {
+                        console.log('fail')
+                        $("input").hasClass("is-valid") ? $("input").removeClass("is-valid") : $("#userSearch").addClass("is-invalid");
+                        console.log($("#countryResult")[0].hidden)
+                        $("#helpText").html("Impossible de trouver un résultat")
+                        $("#countryResult")[0].hidden ? $("#countryResult").show() : () => {};
+                        $("#country").html("")
+                    })
                 }
-
-                currencies +="</ul></li>"
-
-                $("#countriesList").append(`
-                    <li>Pays : ${country.name}</li>
-                    <ul>
-                        <li>Capitale : ${country.capital}
-                        <li>Région : ${country.region} (${country.subregion})
-                        ${currencies}
-                    </ul>
-                `)
-            }
-            $("#countriesList").append("</ul>")
-        });
-
-    
+            }        
+        }
+    })
 });
